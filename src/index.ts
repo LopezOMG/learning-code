c886e9d80b558d241fca6d091b48d569d28fd522
Wilson  Lopez <101467871+LopezOMG@users.noreply.github.com>
2022-12-04 02:21:48 +0000

    changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 type Operacion =  "add" | "substract" | "multiply" | "divide"
 
 const calcular = (a: number, b: number, op: Operacion) => {
+  if(op == "add") return a + b
+  if(op == "substract") return a - b
+  if(op == "multiply") return a * b
   
+  if(op == "divide" && b != 0) return a / b
+
+  throw new Error("Operation not implemented")
 }
